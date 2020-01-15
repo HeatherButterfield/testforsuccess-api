8c9c98fc268bde2742fd2897e0e47338947aa1f3
Corey Berrier <cberrier@deseretdigital.com>
2020-01-15 13:15:14 -0700

    Fixes for build

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object('configurations.DevelopmentConfig')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
