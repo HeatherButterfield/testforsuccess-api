3531b85cf424423a1d40e954745b090671b9edab
Corey Berrier <cberrier@deseretdigital.com>
2020-01-15 13:04:51 -0700

    Trying to get this working

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from templates.api.views import api_blueprint
+# register the blueprints
+app.register_blueprint(api_blueprint)
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Oops! This endpoint isn't ready yet. Try again in V2"
+
+if __name__ == '__main__':
+    app.run()
--- configurations.py
+++ configurations.py
@@ -1,9 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class BaseConfig(object):
         '''
         Base config class
         '''
         DEBUG = True
         TESTING = False
+        SECRET_KEY = 'this-really-needs-to-be-changed'
+        DATABASE_URL = os.environ['DATABASE_URL']
 class ProductionConfig(BaseConfig):
         """
         Production specific config
