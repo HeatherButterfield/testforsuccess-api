c099b303106098d7c0902f8e7587f961b473c9fe
Corey Berrier <cberrier@deseretdigital.com>
2020-03-11 00:41:04 -0600

    Fixed models to use json

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,28 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config.from_object('configurations.DevelopmentConfig')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+db.create_all()
+# import all blueprints
+from .classroom.routes import classroom_blueprint
+from .problems.routes import problems_blueprint
+from .quiz.routes import quiz_blueprint
+from .scores.routes import scores_blueprint
+from .step.routes import step_blueprint
+from .student.routes import student_blueprint
+from .teacher.routes import teacher_blueprint
 
-def create_app():
-    #Initialize the core application
-    app = Flask(__name__)
-    app.config.from_object('configurations.DevelopmentConfig')
-    db.init_app(app)
-    #Initialize Plugins
-    with app.app_context():
-        from .classroom.routes import classroom_blueprint
-        from .problems.routes import problems_blueprint
-        from .quiz.routes import quiz_blueprint
-        from .scores.routes import scores_blueprint
-        from .step.routes import step_blueprint
-        from .student.routes import student_blueprint
-        from .teacher.routes import teacher_blueprint
-
-        db.create_all()
-        # from . import BaseModel
-        # db.make_declarative_base(BaseModel)
-        # register the blueprints
-        app.register_blueprint(classroom_blueprint)
-        app.register_blueprint(problems_blueprint)
-        app.register_blueprint(quiz_blueprint)
-        app.register_blueprint(scores_blueprint)
-        app.register_blueprint(step_blueprint)
-        app.register_blueprint(student_blueprint)
-        app.register_blueprint(teacher_blueprint)
-
-        @app.errorhandler(404)
-        def not_found(e):
-            return "Oops! This endpoint isn't ready yet. Try again in V2"
-
-    return app
+# register the blueprints
+app.register_blueprint(classroom_blueprint)
+app.register_blueprint(problems_blueprint)
+app.register_blueprint(quiz_blueprint)
+app.register_blueprint(scores_blueprint)
+app.register_blueprint(step_blueprint)
+app.register_blueprint(student_blueprint)
+app.register_blueprint(teacher_blueprint)
--- app/teacher/routes.py
+++ app/teacher/routes.py
@@ -1,29 +1,105 @@
-from flask import render_template, Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
+from .models import Teacher, TeacherSchema
 from app import db
-from .models import Teacher
+
 teacher_blueprint = Blueprint('teacher_controller', __name__)
 
 @teacher_blueprint.route('/api/teacher/getAllTeachers/', methods=['GET'])
 def getAllTeachers():
-    teachers = db.session.query(Teacher).all()
-    return jsonify([i.to_dict() for i in teachers])
+    teachers = Teacher.query \
+        .all()
+
+    teacher_schema = TeacherSchema(many=True)
+    return jsonify(teacher_schema.dump(teachers))
 
 @teacher_blueprint.route('/api/teacher/getTeacherById/<int:teacher_id>', methods=['GET'])
 def getTeacherById(teacher_id):
-    teacher = db.session.query(Teacher).get(teacher_id)
-    return jsonify(teacher.to_dict())
+    teacher = Teacher.query \
+        .filter(Teacher.id == teacher_id) \
+        .one_or_none()
 
+    if teacher is not None:
+        teacher_schema = TeacherSchema()
+        return jsonify(teacher_schema.dump(teacher))
+    else:
+        abort(404, 'Teacher not found for Id: {teacher_id}'.format(teacher_id=teacher_id))
 
 @teacher_blueprint.route('/api/teacher/getTeacherByFacultyId/<int:faculty_id>', methods=['GET'])
 def getTeacherByFacultyId(faculty_id):
-    teacher = db.session.query(Teacher).filter(Teacher.faculty_id == faculty_id).first()
-    return jsonify(teacher.to_dict())
+    teacher = Teacher.query \
+        .filter(Teacher.faculty_id == faculty_id) \
+        .one_or_none()
+
+    if teacher is not None:
+        teacher_schema = TeacherSchema()
+        return jsonify(teacher_schema.dump(teacher))
+    else:
+        abort(404, 'Teacher not found for Id: {faculty_id}'.format(faculty_id=faculty_id))
+
+@teacher_blueprint.route('/api/teacher/saveTeacher/', methods=['PUT'])
+def saveTeacher():
+    teacher = request.get_json()
+    faculty_id = teacher.get('faculty_id')
+    fname = teacher.get('first_name')
+    lname = teacher.get('last_name')
+
+    existing_teacher = Teacher.query \
+        .filter(Teacher.first_name == fname) \
+        .filter(Teacher.last_name == lname) \
+        .filter(Teacher.faculty_id == faculty_id) \
+        .one_or_none()
+
+    if existing_teacher is None:
+        schema = TeacherSchema()
+        new_teacher = schema.load(teacher, session=db.session)
+
+        db.session.add(new_teacher)
+        db.session.commit()
+
+        return jsonify({'Result': 'Success'}), 200
+    else:
+        abort(404, f'Faculty Member: {faculty_id} - {fname} {lname} exists already')
 
 @teacher_blueprint.route("/api/teacher/updateTeacher/<int:teacher_id>", methods=['PUT'])
-def users_update(teacher_id):
-    teacher = db.session.query(Teacher).get(teacher_id)
-    form = request.get_json()
-    if not form.get('teacher'):
-        return jsonify({'Error': 'Bad data received'}), 400
+def updateTeacher(teacher_id):
+    update_teacher = Teacher.query \
+        .filter(Teacher.id == teacher_id) \
+        .one_or_none()
 
-    return jsonify({'teacher': teacher.to_dict()}), 200
+    req = request.get_json()
+    faculty_id = req.get('faculty_id')
+    fname = req.get('first_name')
+    lname = req.get('last_name')
+    existing_teacher = Teacher.query \
+        .filter(Teacher.first_name == fname) \
+        .filter(Teacher.last_name == lname) \
+        .filter(Teacher.faculty_id == faculty_id) \
+        .one_or_none()
+
+    if update_teacher is None:
+        abort(404, 'Teacher not found for Id: {teacher_id}'.format(teacher_id=teacher_id))
+    elif (existing_teacher is not None and existing_teacher.id != teacher_id):
+        abort(404, f'Teacher: {teacher_id} - {fname} {lname} exists already')
+    else:
+        schema = TeacherSchema()
+        update = schema.load(req, session=db.session)
+
+        update.id = update_teacher.id
+
+        db.session.merge(update)
+        db.session.commit()
+
+        return jsonify({'Result': 'Success'}), 200
+
+@teacher_blueprint.route("/api/teacher/deleteTeacher/<int:teacher_id>", methods=['DELETE'])
+def deleteTeacher(teacher_id):
+    teacher = Teacher.query \
+        .filter(Teacher.id == teacher_id) \
+        .one_or_none()
+
+    if teacher is not None:
+        db.session.delete(teacher)
+        db.session.commit()
+        return jsonify({'Result': 'Success'}), 200
+    else:
+        abort(404, 'Teacher not found for Id: {teacher_id}'.format(teacher_id=teacher_id))
