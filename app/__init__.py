e8f35697c4788731729715c374532f689ea63148
Corey Berrier <cberrier@deseretdigital.com>
2020-03-15 14:10:43 -0600

    Removed basedir

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config.from_object('configurations.DevelopmentConfig')
 db = SQLAlchemy(app)
